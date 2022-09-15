8b0e4c0b9ace442b306b1172fae3feb49ed4de92
cyoo934 <cyoo934@aucklanduni.ac.nz>
2022-09-15 18:13:33 +1200

    Started on features to switch between profiles,
    CSVwriter and reader will be used, as well as some modification to the
    main menu

--- src/main/java/nz/ac/auckland/se206/controller/MenuController.java
+++ src/main/java/nz/ac/auckland/se206/controller/MenuController.java
@@ -1,13 +1,20 @@
 package nz.ac.auckland.se206.controller;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+
+import com.opencsv.exceptions.CsvException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.user.UserManager;
 
 /**
  * This is the controller for menu, more features are to be added
@@ -18,6 +25,17 @@ public class MenuController {
 
   private Scene scene;
   private Parent root;
+  @FXML private ChoiceBox<String> userChoiceBox;
+  @FXML private Label currentUser;
+  
+  public void initialize() throws URISyntaxException, IOException, CsvException {
+//	  UserManager users = new UserManager();
+	  String user123[] = {"Samuel", "Chan", "Yoo","That's me"};
+	  userChoiceBox.getItems().addAll(user123);
+	  userChoiceBox.setValue(user123[0]);
+	  currentUser.setText(user123[0]);
+	  userChoiceBox.setOnAction(this::setUserLabel);
+  }
 
   @FXML
   private void onStartNewGame(ActionEvent event) {
@@ -31,4 +49,15 @@ public class MenuController {
     }
     scene.setRoot(root);
   }
-}
+  
+  @FXML
+  private void onSwitchUser(ActionEvent event) {
+	  System.out.println("Hi");
+  }
+  
+  public void setUserLabel(ActionEvent event) {
+	  String current = userChoiceBox.getValue();
+	  currentUser.setText(current);
+  }
+  
+ }
