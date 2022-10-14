7bf3dff17343326516db06ebec6ea0fd35b88066
Mattwai <mwai409@aucklanduni.ac.nz>
2022-10-14 16:20:30 +1300

    tidy up layout from merge on main menu page

--- src/main/java/nz/ac/auckland/se206/controller/BadgeController.java
+++ src/main/java/nz/ac/auckland/se206/controller/BadgeController.java
@@ -15,7 +15,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.user.User;
@@ -51,10 +50,6 @@ public class BadgeController {
   @FXML private List<ImageView> fu = new ArrayList<ImageView>();
 
   public void initialize() throws IOException {
-    Image returnImg = new Image("/images/returnIcon.png");
-    ImageView returnImgView = new ImageView(returnImg);
-    menuButton.setGraphic(returnImgView);
-
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     // construct Type that tells Gson about the generic type
     Type userListType = new TypeToken<List<User>>() {}.getType();
